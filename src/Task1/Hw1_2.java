a3d67eeb68900ff7456fb7230d1705e0a1c13de1
smallIt8 <smallIt88@gmail.com>
2024-05-30 20:41:12 +0300

    Обновил все, в том числе и ДЗ1

---
+++ src/Task1/Hw1_2.java
@@ -0,0 +1,159 @@
+package src.Task1;
+
+public class Hw1_2 {
+
+    //объявляю примитивные переменные
+
+    private static byte b;
+    private static short s;
+    private static int i;
+    private static long l;
+    private static float f;
+    private static double d;
+    private static char c;
+    private static boolean bool;
+
+    //объявляю обертки переменных
+
+    private static Byte bOb;
+    private static Short sOb;
+    private static Integer iOb;
+    private static Long lOb;
+    private static Float fOb;
+    private static Double dOb;
+    private static Character cOb;
+    private static Boolean boolOb;
+
+    // создаю геттеры и сеттеры для примитивов
+
+    public byte getB() {
+        return b;
+    }
+
+    public void setB(byte b) {
+        this.b = b;
+    }
+
+    public short getS() {
+        return s;
+    }
+
+    public void setS(short s) {
+        this.s = s;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public long getL() {
+        return l;
+    }
+
+    public void setL(long l) {
+        this.l = l;
+    }
+
+    public float getF() {
+        return f;
+    }
+
+    public void setF(float f) {
+        this.f = f;
+    }
+
+    public double getD() {
+        return d;
+    }
+
+    public void setD(double d) {
+        this.d = d;
+    }
+
+    public char getC() {
+        return c;
+    }
+
+    public void setC(char c) {
+        this.c = c;
+    }
+
+    public boolean isBool() {
+        return bool;
+    }
+
+    public void setBool(boolean bool) {
+        this.bool = bool;
+    }
+
+    // создаю геттеры и сеттеры для оберток
+
+    public Byte getBOb() {
+        return bOb;
+    }
+
+    public void setBOb(Byte bOb) {
+        this.bOb = bOb;
+    }
+
+    public Short getSOb() {
+        return sOb;
+    }
+
+    public void setSOb(Short sOb) {
+        this.sOb = sOb;
+    }
+
+    public Integer getIOb() {
+        return iOb;
+    }
+
+    public void setIOb(Integer iOb) {
+        this.iOb = iOb;
+    }
+
+    public Long getLOb() {
+        return lOb;
+    }
+
+    public void setLOb(Long lOb) {
+        this.lOb = lOb;
+    }
+
+    public Float getFOb() {
+        return fOb;
+    }
+
+    public void setFOb(Float fOb) {
+        this.fOb = fOb;
+    }
+
+    public Double getDOb() {
+        return dOb;
+    }
+
+    public void setDOb(Double dOb) {
+        this.dOb = dOb;
+    }
+
+    public Character getCOb() {
+        return cOb;
+    }
+
+    public void setCOb(Character cOb) {
+        this.cOb = cOb;
+    }
+
+    public Boolean getBoolOb() {
+        return boolOb;
+    }
+
+    public void setBoolOb(Boolean boolOb) {
+        this.boolOb = boolOb;
+    }
+
+}
