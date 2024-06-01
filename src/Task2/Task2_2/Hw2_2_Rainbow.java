10f9f20b768ca47b6fba97826c95e88d498aa7c6
smallIt8 <smallIt88@gmail.com>
2024-06-01 17:20:40 +0300

    ДЗ 3 полное

--- src/Task2/Task2_2/Hw2_2_Rainbow.java
+++ src/Task2/Task2_2/Hw2_2_Rainbow.java
@@ -4,17 +4,20 @@ import java.util.Scanner;
 
 public class Hw2_2_Rainbow {
 
+    // считывает введенные значения пользователем
     private static final Scanner scanner = new Scanner(System.in);
 
-    private String color;
-    private int numColor;
+    // две переменные для конструктора
+    private final String color;
+    private final int numColor;
 
+    // конструктор с двумя полями
     public Hw2_2_Rainbow(String color, int numColor) {
         this.color = color;
         this.numColor = numColor;
     }
 
-
+    // создаем 7 объектов цветов радуги
     private static final Hw2_2_Rainbow RED = new Hw2_2_Rainbow("красный" , 1);
 
     private static final Hw2_2_Rainbow ORANGE = new Hw2_2_Rainbow("оранжевый" , 2);
@@ -29,6 +32,7 @@ public class Hw2_2_Rainbow {
 
     private static final Hw2_2_Rainbow VIOLET = new Hw2_2_Rainbow("фиолетовый" , 7);
 
+    // это геттеры для двух полей объекта, они вроде как не нужны, но не сообразил как без них
     private String getColor() {
          return this.color;
     }
@@ -37,21 +41,60 @@ public class Hw2_2_Rainbow {
          return this.numColor;
     }
 
+    // тут метод для взаимодействия с классом main, т.е. часть через которую все запускается
     public static void start() {
         Hw2_2_Rainbow inputNumColor;
+        Hw2_2_Rainbow inputNumTwoColor;
 
+        // тут цикл, в нем инициализируется введенное первое значение цвета пользователем и...
         do {
             System.out.println("Введите номер цвета от 1 до 7: ");
             int numColor = scanner.nextInt();
+            scanner.nextLine(); // не понимаю как работает, очищает буфер сканера? если нет, то завершается программа!
             inputNumColor = choiseNumColor(numColor);
 
+            // выполняется проверка, что пользователь ввел первое значение из диапазона возможных, и если нет, то возвращает назад в тело цикла
             if (inputNumColor == null) {
                 System.out.println("Введенный номер цвета вне границ используемых цветов от 1 до 7.");
             }
         } while (inputNumColor == null);
+
+        // тут происходит вывод названия первого введенного цвета по номеру цвета
         System.out.println("Введенный цвет " + inputNumColor.getColor());
+
+        System.out.println("Хотите скомбинировать введенный цвет с другим? Введите 'да' или 'нет': ");
+
+        // тут мы считываем введенное строчное значение и приводим к нижнему регистру
+        String combinedColor = scanner.nextLine().toLowerCase();
+
+        // тут мы на основе введенного пользователем ответа, либо добавляем цвет, либо завершаем программу
+        if (combinedColor.equals("да")) {
+            // тут цикл, в нем инициализируется введенное второе значение цвета пользователем и...
+            do {
+                System.out.println("Введите номер второго цвета от 1 до 7: ");
+                int numTwoColor = scanner.nextInt();
+                inputNumTwoColor = choiseNumColor(numTwoColor);
+
+                // выполняется проверка, что пользователь ввел второе значение из диапазона возможных, и если нет, то возвращает назад в тело цикла
+                if (inputNumTwoColor == null) {
+                    System.out.println("Введенный номер второго цвета вне границ используемых цветов от 1 до 7.");
+                }
+            } while (inputNumTwoColor == null);
+
+            // тут происходит вывод названия второго введенного цвета по номеру цвета
+            System.out.println("Введенный второй цвет " + inputNumTwoColor.getColor());
+
+            System.out.println(" ");
+
+            // тут выводим название смешанного цвета
+            System.out.println("Введенный комбинированный цвет " + inputNumColor.getColor() + "-" + inputNumTwoColor.getColor());
+
+        } else if (combinedColor.equals("нет")) {
+            System.out.println("Тогда программа завершается!");
+        }
     }
 
+        // тут метод с оператором свич, где введенный номер цвета пользователем ссылается на объект
         private static Hw2_2_Rainbow choiseNumColor(int numColor) {
             switch (numColor) {
                 case 1:
