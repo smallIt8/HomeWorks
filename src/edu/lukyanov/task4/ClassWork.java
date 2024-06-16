844b43809a48be4650bd35622705f0285c085c7f
smallIt8 <smallIt88@gmail.com>
2024-06-16 13:37:08 +0300

    Обновил все 1

--- src/edu/lukyanov/task4/ClassWork.java
+++ src/edu/lukyanov/task4/ClassWork.java
@@ -18,11 +18,9 @@ public class ClassWork {
         }
         System.out.println("Количество вхождений слова 'test'  в строке 'text': " + count);
 
-
         System.out.println("Первый символ в строке 'text': " + text.charAt(0));
         int length = text.length();
         System.out.println("Последний символ в строке 'text': " +  text.charAt(length-1));
-
 
         if ((text.length()) % 2 == 1) {
             int averagheIndex = text.length()/2;
@@ -38,16 +36,66 @@ public class ClassWork {
             }
         }
 
+        System.out.println("");
+        int maxNumLength = 0;
+        int curNumLength = 0;
+
+        for(int i = 0; i < text.length(); ++i) {
+            char r = text.charAt(i);
+            if (Character.isDigit(r)) {
+                ++curNumLength;
+                if (curNumLength > maxNumLength) {
+                    maxNumLength = curNumLength;
+                }
+            } else {
+                curNumLength = 0;
+            }
+        }
+
+        System.out.println("Наибольшее количество идущих подряд цифр: " + maxNumLength);
+
+        StringBuilder stringBuilder = new StringBuilder(text);
+        stringBuilder.reverse();
+        if (text.equals(stringBuilder.reverse())) {
+            System.out.println("Строка является полиндромной");
+        } else {
+            System.out.println("Строка не является полиндромной");
+        }
+
+        int indexFirstWordLong = -1;
+        int indexLastWordShort = -1;
+        int maxLengthWord = 0;
+        int minLengthWord = text.length();
+
+        for(int i = 0; i < split.length; ++i) {
+            String word = split[i];
+            if (word.length() > maxLengthWord) {
+                maxLengthWord = word.length();
+                indexFirstWordLong = i;
+            }
+        }
+
+        for(int i = 0; i < split.length; ++i) {
+            String word = split[i];
+            if (!word.isEmpty() && word.length() < minLengthWord) {
+                minLengthWord = word.length();
+                indexLastWordShort = i;
+            }
+        }
+
+        if (indexFirstWordLong != -1 && indexLastWordShort != -1) {
+            String temp = split[indexFirstWordLong];
+            split[indexFirstWordLong] = split[indexLastWordShort];
+            split[indexLastWordShort] = temp;
+        }
 
 
+        StringBuilder result = new StringBuilder();
+        for (String word : split) {
+            result.append(word).append(" ");
+        }
 
-
-
-
-
-
-
-
-
+        System.out.println("Поменяли слова местами: " + result.toString());
     }
 }
+
